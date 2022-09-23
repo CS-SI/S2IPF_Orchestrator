b9c37da803dedce62df4cfec13e0415c6db30d81
Esquis Benjamin <benjamin.esquis@csgroup.eu>
2022-09-23 15:04:05 +0200

    Added copyrigh

--- orchestrator/logsetup.py
+++ orchestrator/logsetup.py
@@ -13,6 +13,7 @@
 #         \/            \/     \/     \/     \/                   \/
 #
 #
+#  Copyright (C) 2014-2022 CS GROUP – France, https://www.csgroup.eu
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
